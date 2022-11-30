27310eff1c4a3f76e2328feec8288a77bba1975b
Giggbit <danya19981017@gmail.com>
2022-11-30 11:19:17 +0100

    30.11.2022 homework Мьютексы

--- Main.cpp
+++ Main.cpp
@@ -4,14 +4,24 @@
 #include <tchar.h>
 #include"resource.h"
 #pragma comment(lib,"comctl32")
-#define GCL_HICON (-14)
+
+#define WM_ICON WM_APP
+#define ID_TRAYICON WM_USER
 
 BOOL CALLBACK DlgProc(HWND, UINT, WPARAM, LPARAM);
 
 PNOTIFYICONDATA pNID;
-HWND hList, hTime, hLevel, hDialog;
+HWND hTime, hLevel, hDialog, hText;
+HWND hModalDialog;		
 HICON hIcon;
+HANDLE hMutex;
 TCHAR str_time[50];
+
+TCHAR lvl1[] = TEXT("аоао оаоа ааоо ооаа оаао аооа");
+TCHAR lvl2[] = TEXT("влвл лвлв ввлл ллвв вллв лввл");
+TCHAR lvl3[] = TEXT("прпр рпрп ппрр ррпп рппр пррп");
+TCHAR lvl4[] = TEXT("кеке екек ккее еекк кеек екке");
+TCHAR lvl5[] = TEXT("нгнг гнгн ннгг ггнн нггн гннг");
 
 int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPTSTR lpszCmdLine, int nCmdShow) {
 	return DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc);
@@ -52,44 +62,86 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	switch (message)
 	{
 	case WM_INITDIALOG: {
-		hList = GetDlgItem(hWnd, IDC_LIST1);
-		SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("Первый уровень")));
-		SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("Второй уровень")));
-		SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("Третий уровень")));
-		SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("Четвертый уровень")));
-		SendMessage(hList, LB_ADDSTRING, 0, LPARAM(TEXT("Пятый уровень")));
-
-		hTime = GetDlgItem(hWnd, IDC_TIME); 
+		hTime = GetDlgItem(hWnd, IDC_TIME);
 		hLevel = GetDlgItem(hWnd, IDC_LEVEL);
+		hText = GetDlgItem(hWnd, IDC_TEXT); 
 		hDialog = hWnd;
 
 		// Получим дескриптор экземпляра приложения
 		HINSTANCE hInst = GetModuleHandle(NULL);
 		hIcon = LoadIcon(hInst, MAKEINTRESOURCE(IDI_ICON1)); // загружаем иконку
 		SetClassLong(hDialog, GCL_HICON, LONG(hIcon)); // устанавливаем иконку в главном окне приложения
-		memset(pNID, 0, sizeof(NOTIFYICONDATA)); //Обнуление структуры
-		pNID->cbSize = sizeof(NOTIFYICONDATA); //размер структуры
-		pNID->hIcon = hIcon; //загружаем пользовательскую иконку
-		pNID->hWnd = hWnd; //дескриптор окна, которое будет получать уведомляющие сообщения, ассоциированные с иконкой в трэе.	
+		//memset(pNID, 0, sizeof(NOTIFYICONDATA)); //Обнуление структуры
+		//pNID->cbSize = sizeof(NOTIFYICONDATA); //размер структуры
+		//pNID->hIcon = hIcon; //загружаем пользовательскую иконку
+		//pNID->hWnd = hWnd; //дескриптор окна, которое будет получать уведомляющие сообщения, ассоциированные с иконкой в трэе.	
+		//pNID->uCallbackMessage = WM_ICON;
+		//pNID->uFlags = NIF_TIP | NIF_ICON | NIF_MESSAGE | NIF_INFO;
 
-
+		TCHAR GUID[] = TEXT("{D99CD3E0-670D-4def-9B74-99FD7E793DFB}");
+		hMutex = CreateMutex(NULL, FALSE, GUID);
+		DWORD dwAnswer = WaitForSingleObject(hMutex, 0);
+		if (dwAnswer == WAIT_TIMEOUT) {
+			MessageBox(hWnd, TEXT("Нельзя запускать более одной копии приложения!!!"), TEXT("Тренажер печати на клавиатуре"), MB_OK | MB_ICONINFORMATION);
+			EndDialog(hWnd, 0);
+		}
 	}
 	break;
 
 	case WM_COMMAND: {
 		if (LPARAM(wParam) == IDC_START) {
-			int index = SendMessage(hList, LB_GETCURSEL, 0, 0);
-			if (index != LB_ERR) {
-				DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG2), hWnd, DLGPROC(DlgProc));
+			LRESULT result1 = SendDlgItemMessage(hWnd, IDC_RADIO1, BM_GETCHECK, 0, 0);
+			LRESULT result2 = SendDlgItemMessage(hWnd, IDC_RADIO2, BM_GETCHECK, 0, 0);
+			LRESULT result3 = SendDlgItemMessage(hWnd, IDC_RADIO3, BM_GETCHECK, 0, 0);
+			LRESULT result4 = SendDlgItemMessage(hWnd, IDC_RADIO4, BM_GETCHECK, 0, 0);
+			LRESULT result5 = SendDlgItemMessage(hWnd, IDC_RADIO5, BM_GETCHECK, 0, 0);
+
+			if (result1 == BST_CHECKED) {
+				hModalDialog = CreateDialog(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG2), hWnd, DLGPROC(DlgProc));
+				ShowWindow(hModalDialog, SW_RESTORE);
+				SetWindowText(hText, lvl1);
+				SendMessage(hLevel, WM_SETTEXT, 0, LPARAM(TEXT("1")));
 			}
-			SetTimer(hTime, 1, 3000, NULL);
+			if (result2 == BST_CHECKED) {
+				hModalDialog = CreateDialog(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG2), hWnd, DLGPROC(DlgProc));
+				ShowWindow(hModalDialog, SW_RESTORE);
+				SetWindowText(hText, lvl2);
+				SendMessage(hLevel, WM_SETTEXT, 0, LPARAM(TEXT("2")));
+			}
+			if (result3 == BST_CHECKED) {
+				hModalDialog = CreateDialog(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG2), hWnd, DLGPROC(DlgProc));
+				ShowWindow(hModalDialog, SW_RESTORE);
+				SetWindowText(hText, lvl3);
+				SendMessage(hLevel, WM_SETTEXT, 0, LPARAM(TEXT("3")));
+			}
+			if (result4 == BST_CHECKED) {
+				hModalDialog = CreateDialog(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG2), hWnd, DLGPROC(DlgProc));
+				ShowWindow(hModalDialog, SW_RESTORE);
+				SetWindowText(hText, lvl4);
+				SendMessage(hLevel, WM_SETTEXT, 0, LPARAM(TEXT("4")));
+			}
+			if (result5 == BST_CHECKED) {
+				hModalDialog = CreateDialog(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG2), hWnd, DLGPROC(DlgProc));
+				ShowWindow(hModalDialog, SW_RESTORE);
+				SetWindowText(hText, lvl5);
+				SendMessage(hLevel, WM_SETTEXT, 0, LPARAM(TEXT("5")));
+			}
 		}
+
 	}
 	break;
 
 	/*case WM_TIMER:
 		
 	break;*/
+
+	case WM_SIZE: {
+		if (message == SIZE_MINIMIZED) {
+			ShowWindow(hWnd, SW_HIDE); // Прячем окно
+			Shell_NotifyIcon(NIM_ADD, pNID); // Добавляем иконку в трэй
+		}
+	}
+	break;
 
 	case WM_CLOSE:
 		EndDialog(hWnd, 0);
--- resource.h
+++ resource.h
@@ -12,6 +12,46 @@
 #define IDC_START                       1005
 #define IDC_TIME                        1006
 #define IDC_LEVEL                       1007
+#define IDC_BUTTON1                     1009
+#define IDC_BUTTON2                     1010
+#define IDC_RADIO1                      1010
+#define IDC_BUTTON3                     1011
+#define IDC_RADIO2                      1011
+#define IDC_BUTTON4                     1012
+#define IDC_RADIO3                      1012
+#define IDC_BUTTON5                     1013
+#define IDC_RADIO4                      1013
+#define IDC_BUTTON6                     1014
+#define IDC_RADIO5                      1014
+#define IDC_BUTTON7                     1015
+#define IDC_BUTTON8                     1016
+#define IDC_BUTTON9                     1017
+#define IDC_BUTTON10                    1018
+#define IDC_BUTTON11                    1019
+#define IDC_BUTTON12                    1020
+#define IDC_BUTTON13                    1021
+#define IDC_BUTTON14                    1022
+#define IDC_BUTTON15                    1023
+#define IDC_BUTTON16                    1024
+#define IDC_BUTTON17                    1025
+#define IDC_BUTTON18                    1026
+#define IDC_BUTTON19                    1027
+#define IDC_BUTTON20                    1028
+#define IDC_BUTTON21                    1029
+#define IDC_BUTTON22                    1030
+#define IDC_BUTTON23                    1031
+#define IDC_BUTTON24                    1032
+#define IDC_BUTTON25                    1033
+#define IDC_BUTTON26                    1034
+#define IDC_BUTTON27                    1035
+#define IDC_BUTTON28                    1036
+#define IDC_BUTTON29                    1037
+#define IDC_BUTTON30                    1038
+#define IDC_BUTTON31                    1039
+#define IDC_BUTTON32                    1040
+#define IDC_SPACE                       1041
+#define IDC_EDIT                        1042
+#define IDC_TEXT                        1042
 
 // Next default values for new objects
 // 
@@ -19,7 +59,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_CONTROL_VALUE         1043
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
